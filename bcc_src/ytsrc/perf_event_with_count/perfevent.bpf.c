e1f64fa14e070b2ed5ff57b10caa6d06293f526f
edgarzhou <edgarzhou@tinnove.com.cn>
2024-12-11 09:11:14 +0800

    add perf event with count
    Signed-off-by: edgarzhou <edgarzhou@tinnove.com.cn>

---
+++ bcc_src/ytsrc/perf_event_with_count/perfevent.bpf.c
@@ -0,0 +1,61 @@
+#define BPF_NO_GLOBAL_DATA
+
+// #include <linux/bpf.h>
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+struct perf_bpf_common {
+    int pid;
+    int tid;
+    int count;
+    char comm[16];
+    int diff_ns;
+};
+
+#define DEFINITION_STRUCT_PERF_BPF_COMMON(name) {.pid = 0, .tid=0, .count=0, .diff_ns=0}
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, 32);
+    __type(key, int);
+    __type(value, unsigned int);
+} perf_map SEC(".maps");
+
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, u32);
+} count_map SEC(".maps");
+
+// SEC("kprobe/spidev_ioctl")
+// SEC("tracepoint/block/block_rq_issue")
+SEC("kprobe/do_sys_openat2")
+int bpf_spidev_ioctl(void *ctx){
+     struct perf_bpf_common data = DEFINITION_STRUCT_PERF_BPF_COMMON();
+     long id = bpf_get_current_pid_tgid();
+	 u32 index = 0;
+     u32 *p_count;
+     data.pid = id >> 32;
+     data.tid = (int) id;
+	 bpf_get_current_comm(&data.comm, sizeof(data.comm));
+     
+     p_count = bpf_map_lookup_elem(&count_map, &index);
+    if (p_count) {
+        __sync_fetch_and_add(p_count, 1); // 原子递增计数器
+        data.count = *p_count;
+    } else {
+        long init_val = 1;
+        data.count = init_val;
+        bpf_map_update_elem(&count_map, &index, &init_val, BPF_ANY);
+    }
+
+     
+     bpf_perf_event_output(ctx, &perf_map, BPF_F_CURRENT_CPU, &data, sizeof(data));
+     return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
---
+++ bcc_src/ytsrc/perf_event_with_count/perfevent.c
@@ -0,0 +1,103 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include "perfevent.skel.h"
+#define PERF_BUFFER_PAGES (32)
+struct perf_bpf_common {
+    int pid;
+    int tid;
+    int count;
+    char comm[16];
+    int diff_ns;
+};
+
+static int exiting = 0;
+static void sig_handle_int(int signo)
+{
+	exiting = 1;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	if (level == LIBBPF_DEBUG)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void perf_handle_event(void *ctx, int cpu, void *data, __u32 data_sz){
+    struct perf_bpf_common * bpf_data = (struct perf_bpf_common *)data;
+    printf("pid: %d, tid:%d, comm: %s, count: %d\n", bpf_data->pid, bpf_data->tid, bpf_data->comm, bpf_data->count);
+    return;
+}
+
+static void perf_handle_lost_event(void *ctx, int cpu, __u64 lost_cnt)
+{
+	printf("%s cpu: %d\n", __func__ , cpu);
+}
+
+void enable_trace(void)
+{
+	(void)system("echo 'trace:off' > /sys/kernel/debug/mtkfb");
+	(void)system("echo > /sys/kernel/tracing/set_event");
+	(void)system("echo > /sys/kernel/tracing/trace");
+	(void)system("echo 1 > /sys/kernel/tracing/tracing_on");
+}
+
+int main(int argc, char *argv[]){
+
+    struct perfevent_bpf *skel = NULL;
+    int err = 0, pid = 0;
+    unsigned int index = 0;
+    struct perf_buffer *pb = NULL;
+
+    libbpf_set_print(libbpf_print_fn);
+
+    skel = perfevent_bpf__open_and_load();
+    if(!skel){
+        fprintf(stderr, "Failed to open and load BPF skeleton\n");
+		return 1;
+    }
+
+    /* Attach tracepoint handler */
+	err = perfevent_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+    pb = perf_buffer__new(bpf_map__fd(skel->maps.perf_map), PERF_BUFFER_PAGES, perf_handle_event, perf_handle_lost_event, NULL, NULL);
+    if(!pb){
+        err = -errno;
+        printf("failed to open perf buffer: %d\n", err);
+		goto cleanup;
+    }
+
+    if (signal(SIGINT, sig_handle_int) == SIG_ERR){
+        printf("can't set signal handler: %s\n", strerror(errno));
+		err = 1;
+		goto cleanup;
+    }
+
+    enable_trace();
+    while(!exiting){
+        err = perf_buffer__poll(pb, 100);
+        if(err < 0 && err != -EINTR){
+            printf("error polling perf buffer: %s\n", strerror(-err));
+			goto cleanup;
+        }
+        err = 0;
+    }
+
+cleanup:
+    perf_buffer__free(pb);
+    perfevent_bpf__destroy(skel);
+    return 0;
+}
