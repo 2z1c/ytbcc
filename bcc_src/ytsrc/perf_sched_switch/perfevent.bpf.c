3d851ab2401d5b8e2ecf25459bdf224aa6511a89
edgarzhou <edgarzhou@tinnove.com.cn>
2025-01-06 10:41:15 +0800

    1)  add bpf_perf_event_output update count
    2)  use perf buffer poll api load process_event
    
    Signed-off-by: edgarzhou <edgarzhou@tinnove.com.cn>

---
+++ bcc_src/ytsrc/perf_sched_switch/perfevent.bpf.c
@@ -0,0 +1,84 @@
+#define BPF_NO_GLOBAL_DATA
+
+// #include <linux/bpf.h>
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+struct state_change {
+    u32 pid;
+    pid_t prev_pid;
+    pid_t next_pid;
+    pid_t filter_pid;
+    u64 timestamp;
+    u64 count;
+};
+const volatile pid_t filter_pid = 0;
+
+static u64 fetch_count = 0;
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, 16);
+    __type(key, int);
+    __type(value, u32);
+} events SEC(".maps");
+
+/*******************************************************************************
+cat /sys/kernel/debug/tracing/events/sched/sched_switch/format
+format:
+        field:unsigned short common_type;       offset:0;       size:2; signed:0;
+        field:unsigned char common_flags;       offset:2;       size:1; signed:0;
+        field:unsigned char common_preempt_count;       offset:3;       size:1; signed:0;
+        field:int common_pid;   offset:4;       size:4; signed:1;
+
+        field:char prev_comm[16];       offset:8;       size:16;        signed:0;
+        field:pid_t prev_pid;   offset:24;      size:4; signed:1;
+        field:int prev_prio;    offset:28;      size:4; signed:1;
+        field:long prev_state;  offset:32;      size:8; signed:1;
+        field:char next_comm[16];       offset:40;      size:16;        signed:0;
+        field:pid_t next_pid;   offset:56;      size:4; signed:1;
+        field:int next_prio;    offset:60;      size:4; signed:1;
+
+
+struct trace_event_raw_sched_switch {
+	struct trace_entry ent;
+	char prev_comm[16];
+	pid_t prev_pid;
+	int prev_prio;
+	long int prev_state;
+	char next_comm[16];
+	pid_t next_pid;
+	int next_prio;
+	char __data[0];
+};
+
+******* */
+
+SEC("tracepoint/sched/sched_switch")
+int trace_sched_switch(struct trace_event_raw_sched_switch *ctx) {
+    u64 id = bpf_get_current_pid_tgid();
+    u64 timestamp = bpf_ktime_get_ns();         // 获取当前时间戳（纳秒）
+
+    pid_t prev_pid= 0, next_pid = 0;
+    // 创建事件并提交到 perf buffer
+    struct state_change sc = {};
+    sc.pid = id >> 32;
+    // BPF_CORE_READ(task, start_time);
+    sc.prev_pid = (u32)ctx->prev_pid;
+    sc.next_pid = (u32)ctx->next_pid;
+    // next_pid = BPF_CORE_READ(ctx, next_pid);
+    // bpf_printk("helloworld test_int:%d\n", filter_pid);
+    bpf_printk("%d:%d\n", sc.prev_pid, filter_pid);
+    if (sc.prev_pid == filter_pid || sc.next_pid  == filter_pid) {
+        sc.timestamp = timestamp;
+        // bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &sc, sizeof(sc));
+        __sync_fetch_and_add(&fetch_count, 1);
+        // bpf_printk("prev_pid:%d,  next_pid:%d\n", sc.prev_pid, sc.next_pid);
+        sc.filter_pid = filter_pid;
+        sc.count = fetch_count;
+        bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &sc, sizeof(sc));
+    }
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
---
+++ bcc_src/ytsrc/perf_sched_switch/perfevent.cpp
@@ -0,0 +1,197 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include <iostream>
+#include <cassert>
+#include <unordered_map>
+#include "perfevent.skel.h"
+
+
+typedef unsigned int u32;
+typedef unsigned long long u64;
+#define PERF_BUFFER_PAGES (32)
+struct perf_bpf_common {
+    int pid;
+    int tid;
+    int count;
+    char comm[16];
+    int diff_ns;
+};
+
+static int exiting = 0;
+static void sig_handle_int(int signo)
+{
+	exiting = 1;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+
+// 用于记录进程状态变化的结构体
+struct state_change {
+    u32 pid;
+    pid_t prev_pid;
+    pid_t next_pid;
+    pid_t filter_pid;
+    u64 timestamp;
+    u64 count;
+};
+
+// 用于存储进程状态的时间
+struct state_time {
+    uint64_t sleep_time = 0;
+    uint64_t running_time = 0;
+    uint64_t io_wait_time = 0;
+    uint64_t ready_time = 0;
+};
+
+// 定义一个存储每个进程状态时间的 map
+static std::unordered_map<uint32_t, state_time> process_state_time;
+// void *ctx, int cpu, void *data, __u32 data_sz
+void process_event(void *ctx, int cpu, void *data, __u32 len) {
+    struct state_change *event = (struct state_change*)data;
+    printf("prev_pid -> %d, next_pid-> %d, filter_pid-> %d, count: %lu\n", event->prev_pid, event->next_pid, event->filter_pid, event->count);
+    // uint32_t pid = event->pid;
+    // char prev_state = event->prev_state;
+    // char next_state = event->next_state;
+    // uint64_t timestamp = event->timestamp;
+
+    // // 如果进程不在 map 中，初始化其状态
+    // if (process_state_time.find(pid) == process_state_time.end()) {
+    //     process_state_time[pid] = state_time();
+    // }
+
+    // auto& times = process_state_time[pid];
+
+    // // 计算状态变化的时间差
+    // static uint64_t last_timestamp = 0;
+    // uint64_t duration = 0;
+    // if (last_timestamp != 0) {
+    //     duration = timestamp - last_timestamp;
+    // }
+    // last_timestamp = timestamp;
+
+    // // 根据状态类型更新状态时间
+    // if (prev_state == 'R' && next_state == 'S') {
+    //     // 运行 -> 睡眠
+    //     times.running_time += duration;
+    // } else if (prev_state == 'S' && next_state == 'R') {
+    //     // 睡眠 -> 运行
+    //     times.sleep_time += duration;
+    // } else if (next_state == 'I') {
+    //     // 等待 I/O
+    //     times.io_wait_time += duration;
+    // } else if (next_state == 'R') {
+    //     // 就绪态
+    //     times.ready_time += duration;
+    // }
+}
+
+// 打印进程状态时间
+void print_state_time(uint32_t pid) {
+    if (process_state_time.find(pid) != process_state_time.end()) {
+        auto& times = process_state_time[pid];
+        std::cout << "PID: " << pid << "\n"
+                  << "Running Time: " << times.running_time << " ns\n"
+                  << "Sleep Time: " << times.sleep_time << " ns\n"
+                  << "IO Wait Time: " << times.io_wait_time << " ns\n"
+                  << "Ready Time: " << times.ready_time << " ns\n";
+    }
+}
+
+
+static void perf_handle_event(void *ctx, int cpu, void *data, __u32 data_sz){
+    struct perf_bpf_common * bpf_data = (struct perf_bpf_common *)data;
+    printf("pid: %d, tid:%d, comm: %s, count: %d\n", bpf_data->pid, bpf_data->tid, bpf_data->comm, bpf_data->count);
+    return;
+}
+
+static void perf_handle_lost_event(void *ctx, int cpu, __u64 lost_cnt)
+{
+	printf("%s cpu: %d\n", __func__ , cpu);
+}
+
+void enable_trace(void)
+{
+	assert(!system("echo 'trace:off' > /sys/kernel/debug/mtkfb"));
+	assert(!system("echo > /sys/kernel/tracing/set_event"));
+	assert(!system("echo > /sys/kernel/tracing/trace"));
+	assert(!system("echo 1 > /sys/kernel/tracing/tracing_on"));
+}
+
+int main(int argc, char *argv[]){
+
+    struct perfevent_bpf *skel = NULL;
+    int err = 0;
+    struct perf_buffer *pb = NULL;
+
+    int filter_pid = 0;
+
+    if (argc > 1)
+        filter_pid = atoi(argv[1]);
+    libbpf_set_print(libbpf_print_fn);
+
+    skel = perfevent_bpf__open();
+    if (!skel) {
+        fprintf(stderr, "Failed to open and load BPF skeleton\n");
+        return 1;
+    }
+
+
+    // /* Set PID to trace */
+    skel->rodata->filter_pid = filter_pid;
+    // skel->rodata->filter_tid = filter_tid;
+
+    err = perfevent_bpf__load(skel);
+    if (err) {
+        fprintf(stderr, "Failed to load BPF skeleton\n");
+        perfevent_bpf__destroy(skel);
+        return 1;
+    }
+    /* Attach tracepoint handler */
+	err = perfevent_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+    pb = perf_buffer__new(bpf_map__fd(skel->maps.events), PERF_BUFFER_PAGES, process_event, perf_handle_lost_event, NULL, NULL);
+    if(!pb){
+        err = -errno;
+        printf("failed to open perf buffer: %d\n", err);
+		goto cleanup;
+    }
+
+    if (signal(SIGINT, sig_handle_int) == SIG_ERR){
+        printf("can't set signal handler: %s\n", strerror(errno));
+		err = 1;
+		goto cleanup;
+    }
+
+    enable_trace();
+    while(!exiting){
+        // 1ms
+        err = perf_buffer__poll(pb, 1000 * 1);
+        if(err < 0 && err != -EINTR){
+            printf("error polling perf buffer: %s\n", strerror(-err));
+			goto cleanup;
+        }
+        err = 0;
+    }
+
+cleanup:
+    perf_buffer__free(pb);
+    perfevent_bpf__destroy(skel);
+    return 0;
+}
