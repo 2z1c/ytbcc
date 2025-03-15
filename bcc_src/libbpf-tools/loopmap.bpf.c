3515f98e8ec074055acb00c472ae816e2f3b7472
edgarzhou <edgarzhou@tinnove.com.cn>
2025-03-15 23:36:05 +0800

    loopmap
    Signed-off-by: edgarzhou <edgarzhou@tinnove.com.cn>

---
+++ bcc_src/libbpf-tools/loopmap.bpf.c
@@ -0,0 +1,85 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_endian.h>
+
+#define PERF_MAX_STACK_DEPTH 127
+#define MAX_ENTRIES 8192
+
+typedef unsigned int u32;
+
+struct alloc_info {
+    u64 size;
+    u64 timestamp_ns;
+    // u32 pid;
+    // u32 __reserved[3];
+};
+
+
+struct trace_event_raw_kmalloc {
+        struct trace_entry         ent;                  /*     0     8 */
+        unsigned long              call_site;            /*     8     8 */
+        const void  *              ptr;                  /*    16     8 */
+        size_t                     bytes_req;            /*    24     8 */
+        size_t                     bytes_alloc;          /*    32     8 */
+        unsigned long              gfp_flags;            /*    40     8 */
+        int                        node;                 /*    48     4 */
+        char                       __data[];             /*    52     0 */
+
+        /* size: 56, cachelines: 1, members: 8 */
+        /* padding: 4 */
+        /* last cacheline: 56 bytes */
+};
+
+// struct trace_event_raw_kfree {
+//         struct trace_entry         ent;                  /*     0     8 */
+//         unsigned long              call_site;            /*     8     8 */
+//         const void  *              ptr;                  /*    16     8 */
+//         char                       __data[];             /*    24     0 */
+
+//         /* size: 24, cachelines: 1, members: 4 */
+//         /* last cacheline: 24 bytes */
+// };
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, u64);
+    __type(value, struct alloc_info);
+} allocs SEC(".maps");
+
+
+struct {
+	__uint(type, BPF_MAP_TYPE_STACK_TRACE);
+	__uint(key_size, sizeof(u32));
+    __uint(max_entries, 10240);
+    __uint(value_size, PERF_MAX_STACK_DEPTH * sizeof(u64));
+} stackmap SEC(".maps");
+
+SEC("tracepoint/kmem/kmalloc")
+int trace_kmalloc(struct trace_event_raw_kmalloc *ctx)
+{
+    u64 id = bpf_get_current_pid_tgid();
+    int stack_id = 0;
+    u32 pid = id >> 32;
+    struct alloc_info info = {
+        .size = ctx->bytes_alloc,
+        .timestamp_ns = bpf_ktime_get_ns(),
+    };
+    // stack_id = bpf_get_stackid(ctx, &stackmap, BPF_F_USER_STACK);
+    // if (stack_id >= 0) {
+        bpf_map_update_elem(&allocs, &id, &info, BPF_ANY);
+    // }
+    return 0;
+}
+
+SEC("tracepoint/kmem/kfree")
+int trace_kfree(struct trace_event_raw_kfree *ctx)
+{
+    u64 pid = bpf_get_current_pid_tgid();
+    bpf_map_delete_elem(&allocs, &pid);
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
---
+++ bcc_src/libbpf-tools/loopmap.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include "loopmap.skel.h"
+
+static volatile bool exiting = false;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+
+struct alloc_info {
+    u64 size;
+    u64 timestamp_ns;
+};
+
+static void handle_signal(int sig)
+{
+    exiting = true;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
+    return vfprintf(stderr, format, args);
+}
+
+int main(int argc, char **argv)
+{
+    struct loopmap_bpf *skel;
+    int err;
+
+    /* 处理信号以便在按下 Ctrl-C 时退出 */
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+
+    libbpf_set_print(libbpf_print_fn);
+    /* 打开并加载 eBPF 程序 */
+    // skel = loopmap_bpf__open_and_load();
+    skel = loopmap_bpf__open();
+    if (!skel) {
+        fprintf(stderr, "Failed to open and load BPF skeleton\n");
+        return 1;
+    }
+
+    /* 验证和加载 BPF 程序 */
+    err = loopmap_bpf__load(skel);
+    if (err) {
+        fprintf(stderr, "Failed to load BPF skeleton: %d\n", err);
+        loopmap_bpf__destroy(skel);
+        return 1;
+    }
+
+    /* 附加 BPF 程序到 tracepoints */
+    err = loopmap_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach BPF skeleton: %d\n", err);
+        loopmap_bpf__destroy(skel);
+        return 1;
+    }
+
+    printf("Tracing kmem events... Press Ctrl-C to end.\n");
+
+    while (!exiting) {
+        /* 遍历 allocs 映射 */
+        __u64 key = 0, next_key;
+        struct alloc_info info;
+
+        while (bpf_map_get_next_key(bpf_map__fd(skel->maps.allocs), &key, &next_key) == 0) {
+            if (bpf_map_lookup_elem(bpf_map__fd(skel->maps.allocs), &next_key, &info) == 0) {
+                // if (info.pid == 14606){
+                    printf("PID: %llu, Alloc Size: %llu bytes, Timestamp: %llu ns\n", next_key, info.size, info.timestamp_ns);
+                // }
+            }
+            key = next_key; 
+        }
+
+        sleep(1);
+    }
+
+    /* 清理并销毁 BPF skeleton */
+    loopmap_bpf__destroy(skel);
+    return 0;
+}
